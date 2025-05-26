016fdcd26f8c87f2c8c155554357af2980192ee1
Juan Camilo Penagos Rincon <76062268+CamiloPen@users.noreply.github.com>
2025-05-26 17:51:02 -0500

    Update vistas.sql

--- vistas.sql
+++ vistas.sql
@@ -1,1 +1,4 @@
-SELECT U.firstName, U.lastName, U.email, C.name, U.address FROM user U INNER JOIN city C;
+CREATE VIEW user_city AS
+SELECT U.firstName, U.lastName, U.email, C.name, U.address 
+FROM user U 
+INNER JOIN city C;
